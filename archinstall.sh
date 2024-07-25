39c21f03e8ef0208a64dd207e147113b1f0019e0
Christopher Karras <30561347+ckarras11@users.noreply.github.com>
2024-07-24 17:48:45 -0700

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -1,5 +1,10 @@
+# Install Packages
 yay -S - < pkglist.txt
 
+# Setup ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 chsh -s $(which zsh)
 
+# Setup user for docker
 usermod -aG docker $(whoami)
